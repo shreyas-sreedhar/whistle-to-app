ad964b596987e41999c9e1f2488821d228e517c6
Sreedhar Shreyas <shreyas.sreedhar@gmail.com>
2026-03-27 20:29:42 -0500

    Initial Commit

---
+++ Whistle2/Whistle2App.swift
@@ -0,0 +1,17 @@
+//
+//  Whistle2App.swift
+//  Whistle2
+//
+//  Created by Shreyas Sreedhar on 3/27/26.
+//
+
+import SwiftUI
+
+@main
+struct Whistle2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
